fd47efb31835fdf66231ce0548a91a7d146141ea
Martin Tirion <mtirion@live.nl>
2018-02-15 17:42:43 +0100

    Added Mixed Reality Toolkit as a sub module. Also updated the Hud code to the new version of MRTK. New install instructions are in the README

---
+++ Assets/Scripts/TextTyper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTyper : MonoBehaviour
+{
+    // Time taken for each letter to appear (The lower it is, the faster each letter appear)
+    public float LetterDelay = 0.01f;
+    // Message that will displays till the end that will come out letter by letter
+    public string Message;
+    // Text for the message to display
+    private Text TextControl;
+
+    // Use this for initialization
+    void Start()
+    {
+        TextControl = GetComponent<Text>();
+    }
+
+    public void TypeText(string text, bool refresh = true)
+    {
+        Message = text;
+        if (refresh) TextControl.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in Message.ToCharArray())
+        {
+            TextControl.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(LetterDelay);
+        }
+    }
+}
